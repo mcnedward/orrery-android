b7b7dae03e70c6cbef92e6fed3d104441d99aff8
mcnedward <edwardmcn64@gmail.com>
2015-12-20 01:36:08 +0000

    Update buttons

--- app/src/main/java/com/mcnedward/orrery/view/OrreryMenuView.java
+++ app/src/main/java/com/mcnedward/orrery/view/OrreryMenuView.java
@@ -2,25 +2,32 @@ package com.mcnedward.orrery.view;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 import com.mcnedward.orrery.R;
 import com.mcnedward.orrery.model.Space;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by edward on 17/12/15.
  */
 public class OrreryMenuView extends LinearLayout {
+    private static String TAG = "OrreryMenuView";
 
     private Space space;
     private Context context;
-    private View thisView;
 
     public OrreryMenuView(Space space, Context context) {
         super(context);
@@ -30,19 +37,20 @@ public class OrreryMenuView extends LinearLayout {
         setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
         setOrientation(LinearLayout.HORIZONTAL);
         setGravity(Gravity.CENTER_VERTICAL);
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        thisView = inflater.inflate(R.layout.view_orrery_menu, this, true);
+        inflate(context, R.layout.view_orrery_menu, this);
 
-        setButtonActions();
+        initializeButtons();
     }
 
-    private void setButtonActions() {
-        setClearButtonAction();
+    private void initializeButtons() {
+        RadioButton btnCreate = (RadioButton) findViewById(R.id.btnCreate);
+        RadioButton btnResize = (RadioButton) findViewById(R.id.btnResize);
+        RadioButton btnDelete = (RadioButton) findViewById(R.id.btnDelete);
+        Button btnClear = (Button) findViewById(R.id.btnClear);
+        setClearButtonAction(btnClear);
     }
 
-    private void setClearButtonAction() {
-        Button button = (Button) thisView.findViewById(R.id.btnClear);
+    private void setClearButtonAction(Button button) {
         button.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
